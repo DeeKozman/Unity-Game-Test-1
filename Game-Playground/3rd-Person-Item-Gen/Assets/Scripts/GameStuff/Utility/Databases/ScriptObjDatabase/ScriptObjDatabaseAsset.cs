45c25e0a797e1b66dc6961bd66d3088846c3a81e
DeeKozman <dankoscielak@gmail.com>
2017-02-20 14:52:15 -0500

    Re-added Databases/AbstractDatabase.cs + Others

---
+++ Game-Playground/3rd-Person-Item-Gen/Assets/Scripts/GameStuff/Utility/Databases/ScriptObjDatabase/ScriptObjDatabaseAsset.cs
@@ -0,0 +1,49 @@
+﻿using GameStuff.Interfaces;
+using System.Collections;
+using System;
+using UnityEngine;
+
+namespace GameStuff.Utility.Databases.ScriptObjDatabase
+{
+    [System.Serializable]
+    public class ScriptObjDatabaseAsset : ScriptableObject, IDatabaseAsset
+    {
+        [SerializeField]
+        private int _id;
+
+        [SerializeField]
+        private string _name;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public ScriptObjDatabaseAsset()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+        }
+
+        public ScriptObjDatabaseAsset(int id)
+        {
+            this.Id = id;
+            this.Name = string.Empty;
+        }
+
+        public ScriptObjDatabaseAsset(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+
+}
+
